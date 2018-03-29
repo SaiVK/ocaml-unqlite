b99a306a15f6bbcfb19d14e34c8eac8d31ee70e8
Markus Rudy <webmaster@burgerdev.de>
2018-03-29 22:55:56 +0200

    more functions for unqlite executable

--- bin/main.ml
+++ bin/main.ml
@@ -5,9 +5,9 @@ let bin_version = "0.1"
 
 open Unqlite
 
-let store db key value _ =
-  Logs.debug (fun m -> m "Storing key [%s] in database [%s]." key db);
-  let db = open_db db in
+let store db_name key value _ =
+  Logs.debug (fun m -> m "Storing key [%s] in database [%s]." key db_name);
+  let db = open_db db_name in
   try
     store db key value;
     close db
@@ -17,13 +17,31 @@ let store db key value _ =
 let fetch db_name key _ =
   Logs.debug (fun m -> m "Fetching key [%s] from database [%s]." key db_name);
   let db = open_db db_name in
-  begin
-    match fetch db key with
-    | Some value -> Printf.printf "%s\n" value
-    | None ->
-      Logs.err (fun m -> m "Key [%s] does not exist in database [%s]." key db_name)
-  end;
-  close db
+
+  try
+    fetch_exn db key |> print_endline;
+    close db
+  with
+  | e -> close db; raise e
+
+let delete db_name key _ =
+  Logs.debug (fun m -> m "Deleting key [%s] from database [%s]." key db_name);
+  let db = open_db db_name in
+  try
+    delete_exn db key;
+    close db
+  with
+  | Not_found -> close db
+  | e -> close db; raise e
+
+let append db_name key value _ =
+  Logs.debug (fun m -> m "Appending key [%s] to database [%s]." key db_name);
+  let db = open_db db_name in
+  try
+    append db key value;
+    close db
+  with
+  | e -> close db; raise e
 
 
 (* Logging stuff, copy pasta from docs *)
@@ -56,21 +74,30 @@ let value_term =
 
 (* Cmdliner stuff *)
 
-let fetch_cmd =
-  let doc = "Fetch a value from the database." in
-  Term.(const fetch $ db_term $ key_term $ log_term),
-  Term.info "fetch" ~doc ~sdocs:Manpage.s_common_options
+let tag doc =
+  let word = match String.index_opt doc ' ' with
+    | None -> doc
+    | Some n -> String.sub doc 0 n
+  in String.lowercase_ascii word
 
-let store_cmd =
-  let doc = "Store a key/value pair in the database." in
-  Term.(const store $ db_term $ key_term $ value_term $ log_term),
-  Term.info "store" ~doc ~sdocs:Manpage.s_common_options
+let unary_cmd f t doc =
+  Term.(const f $ db_term $ t $ log_term),
+  Term.info (tag doc) ~doc ~sdocs:Manpage.s_common_options
+
+let binary_cmd f t u doc =
+  Term.(const f $ db_term $ t $ u $ log_term),
+  Term.info (tag doc) ~doc ~sdocs:Manpage.s_common_options
 
 let default_cmd =
   let doc = "Unqlite key/value store" in
   Term.(ret (const (fun _ -> `Help (`Pager, None)) $ log_term)),
   Term.info bin_name ~version:bin_version ~doc
 
-let cmds = [fetch_cmd; store_cmd]
+let cmds = [
+  unary_cmd fetch key_term "Fetch a value from the database.";
+  binary_cmd store key_term value_term "Store a key/value pair in the database.";
+  binary_cmd append key_term value_term "Append a key/value pair to the database.";
+  unary_cmd delete key_term "Delete a key/value pair from the database (if it exists)."
+]
 
 let () = Term.(exit @@ eval_choice default_cmd cmds)
