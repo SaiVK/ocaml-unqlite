884c0140667ddba68682355c0c49225e3ba06df6
Markus Rudy <webmaster@burgerdev.de>
2018-04-02 20:43:15 +0200

    better names for tests (fixes #11)

--- test/test_unqlite_bindings.ml
+++ test/test_unqlite_bindings.ml
@@ -3,8 +3,6 @@ open Fmt
 
 open Unqlite.Bindings
 
-(* TODO: remove hard-coded file locations *)
-
 let assert_raises_unqlite ?(code=None) regexp =
   let none = Fmt.(const string "_") in
   let expected =
@@ -37,36 +35,47 @@ let temp_db opener =
 
 let open_inmem () = open_create ":mem:"
 
-let test1 _ =
+let test_open _ =
   temp_db open_create |> close
 
-let test2 _ =
+let test_basics_disk _ =
   let db = temp_db open_create in
   rollback db;
   commit db;
   close db
 
-let test3 _ =
+let test_basics_mem _ =
   let db = open_inmem () in
   rollback db;
   commit db;
   close db
 
-let test4 _ =
-  let (key, value) = "foo", "bar" in
-  let db = open_inmem () in
+let test_crud db =
+  let (key, value, value2) = "foo", "bar", "baz" in
   store db key value;
   assert_equal value (fetch db key);
+  append db key value;
+  assert_equal (String.concat "" [value; value]) (fetch db key);
+  store db key value2;
+  assert_equal value2 (fetch db key);
   delete db key;
   assert_raises Not_found (fun _ -> fetch db "foo");
   close db
 
-let test5a _ =
+let test_crud_disk _ =
+  let db = open_inmem () in
+  test_crud db
+
+let test_crud_mem _ =
+  let db = open_inmem () in
+  test_crud db
+
+let test_invalid_file _ =
   assert_raises_unqlite ~code:(Some UNQLITE_IOERR) "IO error" @@ fun _ ->
   let db = open_create "/invalid/file/burgerdev.de/ocaml-unqlite" in
   store db "foo" "bar"
 
-let test5b _ =
+let test_open_readwrite _ =
   let _ = Filename.temp_file  in
   let name = temp_db @@ fun x -> x in
   let (key, value) = "5b", "b5" in
@@ -91,17 +100,6 @@ let test5b _ =
   assert_equal ~pp_diff value (fetch db key);
   close db
 
-let test6 _ =
-  let key = "foo" in
-  let value = "bar" in
-  let db = open_inmem () in
-  assert_raises Not_found (fun _ -> fetch db key);
-  store db key value;
-  assert_equal value (fetch db key);
-  append db key value;
-  assert_equal (String.concat "" [value; value]) (fetch db key);
-  close db
-
 let test_cursor _ =
   let key = "foo" in
   let value = "bar" in
@@ -121,13 +119,13 @@ let test_cursor _ =
 
 
 let suite =
-  "unqlite suite" >::: [ "test1" >:: test1
-                       ; "test2" >:: test2
-                       ; "test3" >:: test3
-                       ; "test4" >:: test4
-                       ; "test5a" >:: test5a
-                       ; "test5b" >:: test5b
-                       ; "test6" >:: test6
+  "unqlite suite" >::: [ "test_open" >:: test_open
+                       ; "test_basics_disk" >:: test_basics_disk
+                       ; "test_basics_mem" >:: test_basics_mem
+                       ; "test_crud_disk" >:: test_crud_disk
+                       ; "test_crud_mem" >:: test_crud_mem
+                       ; "test_invalid_file" >:: test_invalid_file
+                       ; "test_open_readwrite" >:: test_open_readwrite
                        ; "test_cursor" >:: test_cursor
                        ]
 
